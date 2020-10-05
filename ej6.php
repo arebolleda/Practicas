c7a30765994d1dbba8eca05dfa0be5cac81fb705
msg1999 <71818408+msg1999@users.noreply.github.com>
2020-10-05 09:40:44 +0200

    Add files via upload

---
+++ ej6.php
@@ -0,0 +1,90 @@
+<!--Ejercicio-6-->
+<!--Marco Santiago-->
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>
+           ej6
+        </title>
+        <link rel="icon" type="image/png" href="media/">
+    </head>
+    <body>
+        <div class="">
+            <?php
+                $nif=53687598;
+                $aux=$nif/23;
+                switch ($aux){
+                    if ($aux == 0)
+                        echo "La letra es: T";
+                        break;
+                    if ($aux == 1)
+                        echo "La letra es: R";
+                        break;
+                    if ($aux == 2)
+                        echo "La letra es: W";
+                        break;
+                    if ($aux == 3)
+                        echo "La letra es: A";
+                        break;
+                    if ($aux == 4)
+                        echo "La letra es: G";
+                        break;
+                    if ($aux == 5)
+                        echo "La letra es: M";
+                        break;
+                    if ($aux == 6)
+                        echo "La letra es: Y";
+                        break;
+                    if ($aux == 7)
+                        echo "La letra es: F";
+                        break;
+                    if ($aux == 8)
+                        echo "La letra es: P";
+                        break;
+                    if ($aux == 9)
+                        echo "La letra es: D";
+                        break;
+                    if ($aux == 10)
+                        echo "La letra es: X";
+                        break;
+                    if ($aux == 11)
+                        echo "La letra es: B";
+                        break;
+                    if ($aux == 12)
+                        echo "La letra es: N";
+                        break;
+                    if ($aux == 13)
+                        echo "La letra es: J";
+                        break;
+                    if ($aux == 14)
+                        echo "La letra es: Z";
+                        break;
+                    if ($aux == 15)
+                        echo "La letra es: S";
+                        break;
+                    if ($aux == 16)
+                        echo "La letra es: Q";  
+                        break;
+                    if ($aux == 17)
+                        echo "La letra es: V";
+                        break;
+                    if ($aux == 18)
+                        echo "La letra es: H";
+                        break;
+                    if ($aux == 19)
+                        echo "La letra es: L";
+                        break;
+                    if ($aux == 20)
+                        echo "La letra es: C";
+                        break;
+                    if ($aux == 21)
+                        echo "La letra es: K";
+                        break;
+                    if ($aux == 22)
+                        echo "La letra es: E";
+                        break;
+                }
+?>
+</div>
+</body>
+</html>
